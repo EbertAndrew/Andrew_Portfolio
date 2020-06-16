34ef13f904a91197d0d92177ae93ae5fa8a3e01f
EbertAndrew <andrewsebertii@gmail.com>
2020-06-16 14:34:05 -0500

    ch

--- main.js
+++ main.js
@@ -2,35 +2,35 @@
 // and also takes a number ( n ) . It then returns
 // your string back to you n number of times.
 
-var favWord;
+var favWord ;
 var favNumb;
 var favWordTimes = [];
 var myArray = [];
 var numberOfTimes = 1;
 
-function stringNumberOfTimes()  {
-    /**
-     * *Grabbing values from the HTML document.
-     */
+function stringNumberOfTimes() {
+	/**
+	   * *Grabbing values from the HTML document.
+	   */
 
-    favWord = document.getElementById("favword").value;
-    document.getElementById("favwordValue").innerHTML = favWord;
-    favNumb = document.getElementById("favnumb").value;
-    document.getElementById("favnumbValue").innerHTML = favNumb;
+	favWord = document.getElementById('favword').value
+	document.getElementById('favwordValue').innerHTML = favWord
+	favNumb = document.getElementById('favnumb').value
+	document.getElementById('favnumbValue').innerHTML = favNumb
 
     /**
      * Add standard gaming logic
      * === is strictly equal, the data type and the value must match.
      */
-    if ( favNumb === null ){
-        favNumb = numberOfTimes ; 
-    }else {
-        favWordTimes = favWord.repeat(favNumb);
-    }
-    document.getElementById("favwordTimesvalue").innerHTML = favWordTimes;
+	if (favNumb === null) {
+		favNumb = numberOfTimes
+	} else {
+		favWordTimes = favWord.repeat(favNumb)
+	}
+	document.getElementById('favwordTimesvalue').innerHTML = favWordTimes
 }
 
-function makeAStringSandwich() {
-    stringNumberOfTimes();
-    console.log();
+function makeAStringSandwich () {
+	stringNumberOfTimes();
+	console.log();
 }
